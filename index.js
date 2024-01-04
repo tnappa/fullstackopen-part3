6a4c83e5daa503e1d2fd4b510f3d200b9b708157
Tuomo Nappa <tuomo.nappa@gmail.com>
2024-01-04 23:44:52 +0200

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('post-data', function (req, res) { return req.method === 'POST' ? JSON.stringify(req.body) : "" })
