eed701db980c96548e11f7abc522fd9ec4d9a3ce
Tuomo Nappa <tuomo.nappa@gmail.com>
2024-01-06 22:38:42 +0200

    exercises done till 3.14

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.evmah51.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  console.log("phonebook:")
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person['name'] + ' ' + person['number'])
+    });
+    mongoose.connection.close()
+  })
+} 
+else if (process.argv.length == 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+  
+  person.save().then(result => {
+    console.log("added %s number %s to phonebook", name, number)
+    mongoose.connection.close()
+  })
+}
+else {
+  console.log('invalid number of arguments')
+  mongoose.connection.close()
+}
